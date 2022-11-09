7835677a89c983107c771cb19adace56b7465d2e
RusAl87 <117069259+RusAl87@users.noreply.github.com>
2022-11-09 08:38:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1.
+
+// let r = 20
+// let g = 40
+// let b = 50
+// console.log(r,g,b)
+
+// 2.
+
+// let price = 100
+// if(price > 100) {
+//    alert("больше")
+// } else if(price < 100) {
+//   alert("меньше")
+// } else {
+//   alert("равно")
+// }
